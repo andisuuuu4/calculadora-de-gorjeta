1f922999f2d40b615c2559405f81771a44ef5697
Anderson Luiz <ansudu015@gmail.com>
2024-03-28 02:54:27 -0300

    quarto commit do JS

--- script.js
+++ script.js
@@ -30,10 +30,10 @@ function receberQuantidadePessoas(evento) {
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao=> {
-    botao.addEventListener("click", receberPorcentagemBotao)
+    botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagemBotao (evento) {
+function receberPorcentagem(evento) {
     botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
 
@@ -42,8 +42,16 @@ function receberPorcentagemBotao (evento) {
         }
     })
 
+  
 
+    if(evento.target.value !== "") {
     Porcentagem = parseFloat(evento.target.value) / 100
+    }else{
+        Porcentagem = 0
+    }
 
     console.log(Porcentagem)
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
